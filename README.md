684efa866936075d39fc70a669eddec770d57094
ugrasenanv <36364057+ugrasenanv@users.noreply.github.com>
2023-06-24 13:17:18 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Input-Edit-and-clear
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-krcugu)
---
+++ app/input-clearable-example.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+/**
+ * @title Input edut and  clear button
+ */
+@Component({
+  selector: 'input-clearable-example',
+  templateUrl: './input-clearable-example.html',
+  styleUrls: ['./input-clearable-example.css'],
+})
+export class InputClearableExample {
+  value = 'Edit and Clear';
+}
